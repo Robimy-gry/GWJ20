22d07a32ff6b87de73b81c366f9c75941970ed12
yaqb1 <32592543+yaqb1@users.noreply.github.com>
2020-04-11 19:54:19 +0200

    Created project

---
+++ Actor.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+var creatureClass = load("res://Creature.gd")
+var creature = creatureClass.new()
+
+func _physics_process(delta):
+	move_and_slide(creature.motion, creature.UP)
+	creature.movement()
+	if is_on_floor():
+		creature.jump()
+	
+	
+
+	
