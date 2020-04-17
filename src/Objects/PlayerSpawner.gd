c18a6c54ffe93e373313c02ba03f252623dc37e2
Edwin Żurawik <ezurawik@poczta.fm>
2020-04-17 18:58:05 +0200

    Added player death after touching enemy

--- src/Objects/PlayerSpawner.gd
+++ src/Objects/PlayerSpawner.gd
@@ -20,3 +20,7 @@ func instance_player():
         print("Please check: PlayerSpawner.gd -> instance_player()")
         player = player_human_scene.instance()
     add_child(player)
+
+func get_coordinates() -> void:
+    self.position = get_child(0).global_position
+    
--- src/World.gd
+++ src/World.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var playerTypes = ["Human", "Wasp", "Worm", "Plant"]
-var playerType = "Player"
+var playerType = "Human"
 
 
 func _ready() -> void:
