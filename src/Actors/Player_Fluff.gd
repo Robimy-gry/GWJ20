4b901fd3c6e66130a16ea57b2b5b9a221eea9dbf
Edwin Żurawik <ezurawik@poczta.fm>
2020-04-17 23:56:44 +0200

    Plant and Worm Spritesheets:
    -Added spritesheets for Plant and Worm
    -Added Player_Plant (and player_fluff) + Enemy_Plant
    -Implemented all plant functionalities (try to jump when changed to
    plant)

--- src/Actors/Player_Fluff.gd
+++ src/Actors/Player_Fluff.gd
@@ -1,42 +1,39 @@
 extends Actor
-class_name Player
+
 
 func _ready() -> void:
     add_to_group("Player")
-
+    self.speed.x = 30
+    self.speed.y = -25
+    self.gravity = 5
+    _velocity.y = self.speed.y
 
 func _physics_process(delta: float) -> void:
-    var is_jump_interrupted: = Input.is_action_just_released("ui_up") and _velocity.y < 0.0
     var direction: = get_direction() 
     flip_sprite(direction)
-    _velocity = calculate_move_velocity(_velocity,direction, speed, is_jump_interrupted)
+    _velocity = calculate_move_velocity(_velocity,direction, speed)
     _velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+    if $RayCast2D.is_colliding():
+        die()
 
 
 func get_direction() -> Vector2:
     return Vector2(
         Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-        -1.0 if Input.is_action_just_pressed("ui_up") and is_on_floor() else 1.0
+        0
     )
 
 
 func calculate_move_velocity(
         linear_velocity: Vector2,
         direction: Vector2,
-        speed: Vector2,
-        is_jump_interrupted: bool
+        speed: Vector2
     ) -> Vector2:
     var out: = linear_velocity
     out.x = speed.x * direction.x
     out.y += gravity * get_physics_process_delta_time()
-    if direction.y == -1.0:
-        out.y = speed.y * direction.y
-    if is_jump_interrupted:
-        out.y = 0.0
     return out
 
-func get_camera_instance():
-    return $Camera2D
 
 func _on_Area2D_body_entered(body: Node) -> void:
     killed_by(body)
---
+++ src/Actors/Player_Plant.gd
@@ -0,0 +1,45 @@
+extends Actor
+
+var hasFired = false
+var player_fluff_scene = preload("res://src/Actors/Player_Fluff.tscn")
+
+
+func _ready() -> void:
+    add_to_group("Player")
+    
+
+func _physics_process(delta: float) -> void:
+    
+    if Input.is_action_just_pressed("ui_up") and hasFired != true:
+        fire_fluff()
+        hasFired = true
+        
+    _velocity = calculate_move_velocity(_velocity)
+    _velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+
+
+func fire_fluff():
+    $Camera2D.queue_free()
+    var player = player_fluff_scene.instance()
+    get_parent().add_child(player)
+    remove_from_group("Player")
+    $FluffTimer.start()
+
+
+func calculate_move_velocity(
+        linear_velocity: Vector2
+    ) -> Vector2:
+    var out: = linear_velocity
+    out.x = 0
+    out.y += gravity * get_physics_process_delta_time()
+
+    return out
+    
+
+
+func _on_Area2D_body_entered(body: Node) -> void:
+    killed_by(body)
+
+
+func _on_FluffTimer_timeout() -> void:
+    die()
