37a6173cc944b0034d94fa771ec7433676df4119
KsavinN <34033913+KsavinN@users.noreply.github.com>
2020-04-17 19:22:55 +0200

    Revert "Camera2D for Player_Human (#7)" (#10)
    This reverts commit e1c939fe10e5e5e1a478a2ee504d4fd5e288b5d5.

--- src/World.gd
+++ src/World.gd
@@ -3,8 +3,10 @@ extends Node
 var playerTypes = ["Human", "Wasp", "Worm", "Plant"]
 var playerType = "Wasp"
 
+
 func _ready() -> void:
     pass
 
 func _physics_process(delta: float) -> void:
-			pass
+            pass
+            
